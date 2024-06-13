0b39bba57002c367fe44d9a50359f755e768da4b
Ereyomi Funmilola <funmilolaereyomi@gmail.com>
2024-06-14 00:37:26 +0100

    update

--- src/Projects.js
+++ src/Projects.js
@@ -2,77 +2,73 @@ const ProjectData = [
   {
     id: 1,
     imgSrc: "/Images/Kanban.png",
-    title: "Project Title 10",
-    description: "Description of project 10.",
+    title: "Kanban Board App",
+    description: "A project management tool for organizing tasks into boards, columns, and cards.",
     link: "https://kan-ban-app-react.vercel.app/"
   },
   {
     id: 2,
     imgSrc: "/Images/FeedbackHub.png",
-    title: "Project Title 9",
-    description: "Description of project 9.",
+    title: "Feedback Hub",
+    description: "A platform for collecting feedback from users and stakeholders, facilitating communication and collaboration.",
     link: "https://feedbackhub-frontend.vercel.app/"
   },
-  
-  
   {
     id: 3,
     imgSrc: "/Images/e-commerce.png",
-    title: "Project Title 3",
-    description: "Description of project 3.",
+    title: "E-commerce Website",
+    description: "An online platform for buying and selling products, providing a seamless shopping experience.",
     link: "https://varkala.vercel.app/"
   },
   {
     id: 4,
     imgSrc: "/Images/parallax.png",
-    title: "Project Title 4",
-    description: "Description of project 4.",
+    title: "Parallax Scrolling Website",
+    description: "A website design technique that creates an illusion of depth by moving background and foreground elements at different speeds.",
     link: "https://ereyomif.github.io/parallax-scroll-ICIP/"
   },
   {
     id: 5,
     imgSrc: "/Images/facebook.png",
-    title: "Project Title 5",
-    description: "Description of project 5.",
+    title: "Facebook Profile Clone",
+    description: "A recreation of the Facebook user profile interface, showcasing HTML and CSS skills.",
     link: "https://ereyomif.github.io/facebook-profile/"
   },
   {
     id: 6,
     imgSrc: "/Images/javaquiz.png",
-    title: "Project Title 6",
-    description: "Description of project 6.",
+    title: "Dynamic JavaScript Quiz",
+    description: "A quiz application built with JavaScript that dynamically generates questions and evaluates user responses.",
     link: "https://ereyomif.github.io/dynamic.js.quiz.ICIP/"
   },
   {
     id: 7,
     imgSrc: "/Images/newsletter.png",
-    title: "Project Title 7",
-    description: "Description of project 7.",
+    title: "Newsletter Signup Form",
+    description: "A simple newsletter signup form integrated with backend services for email subscription management.",
     link: "https://ereyomif.github.io/news-letter/"
   },
   {
     id: 8,
     imgSrc: "/Images/resultcard.png",
-    title: "Project Title 8",
-    description: "Description of project 8.",
+    title: "Student Result Card",
+    description: "A web-based application for viewing and managing student academic records and performance.",
     link: "https://ereyomif.github.io/result-card/"
   },
-{
+  {
     id: 9,
     imgSrc: "/Images/Netflix.png",
-    title: "Project Title 2",
-    description: "Description of project 2.",
+    title: "Netflix Landing Page",
+    description: "A clone of the Netflix homepage, featuring responsive design and interactive elements.",
     link: "https://ereyomif.github.io/Netflix-page/home.html"
   },
-
-{
+  {
     id: 10,
     imgSrc: '/Images/blog.png',
-    title: "Project Title 1",
-    description: "Blog webpage",
+    title: "Personal Blog",
+    description: "A blog website showcasing personal interests, thoughts, and experiences, with articles on various topics.",
     link: "https://ereyomif.github.io/blog-page/"
   },
-  
 ];
 
 export default ProjectData;
